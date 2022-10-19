b939d4de582276bdcaecc8ecbd621c4b6ee746fb
Willian Dos Reis De Souza <willian.souza@sistemafiep.org.br>
2022-10-19 14:55:59 -0300

    primeiro commit

---
+++ aula.php
@@ -0,0 +1,32 @@
+<?php 
+
+echo "Primeira aula de PHP!";
+
+$velocidade = 80;
+$marca = "Fusca";
+$altura = 1.75;
+$brasil_campeao = false;
+
+
+echo "<br>";
+var_dump($velocidade);
+
+echo "<br>";
+var_dump($marca);
+
+echo "<br>";
+var_dump($altura);
+
+echo "<br>";
+var_dump($brasil_campeao);
+
+echo "<br>";
+echo $marca;
+
+$n1 = 50;
+$n2 = 100;
+
+// + - * /
+$soma = $n1 + $n2;
+
+echo "<br>A soma corresponde: ".$soma. " ola mundo";
---
+++ form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aula 1 de formulário</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>Nome</td>
+                <td>
+                    <input type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>Idade</td>
+                <td>
+                    <input type="number">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Enviar">
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
